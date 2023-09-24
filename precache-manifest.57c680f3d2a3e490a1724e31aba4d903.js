c85e35d55226b123728c68b1252e21df782a4340
Kaushik Samadder <kaushiksamadder2003@gmail.com>
2023-09-25 03:08:03 +0530

    hahah

---
+++ precache-manifest.57c680f3d2a3e490a1724e31aba4d903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0f69b6db99530df547bc5abbb9799e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32589d82e0ffde2f254e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32589d82e0ffde2f254e",
+    "url": "/static/js/main.c0ebb627.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
